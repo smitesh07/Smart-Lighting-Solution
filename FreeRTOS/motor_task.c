48858f6a0198ea5b35254a64fbd84c545a358874
Smitesh Modak <smitesh.modak@colorado.edu>
2019-04-29 00:49:05 -0600

    Adding Motor control with GPIO

--- FreeRTOS/motor_task.c
+++ FreeRTOS/motor_task.c
@@ -23,12 +23,46 @@
 //****************************************************************************
 uint32_t g_ui32SysClock;
 
+xTaskHandle motorTaskHandle;
+
+//*****************************************************************************
+//
+// Initialize GPIO for motor control
+//
+//*****************************************************************************
+static void gpioInit(void) {
+    //
+    // Enable the GPIO pin for the Motor (PL2).  Set the direction as output, and
+    // enable the GPIO pin for digital function.
+    //
+    SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOL);
+
+    //
+    // Check if the peripheral access is enabled.
+    //
+    while(!SysCtlPeripheralReady(SYSCTL_PERIPH_GPIOL))
+    {
+    }
+
+    GPIOPinTypeGPIOOutput(GPIO_PORTL_BASE, GPIO_PIN_2);
+    GPIOPadConfigSet(GPIO_PORTL_BASE,GPIO_PIN_2,GPIO_STRENGTH_2MA,GPIO_PIN_TYPE_STD_WPD);
+}
+
 //*****************************************************************************
 //
 // Motor task.
 //
 //*****************************************************************************
 void motorTask( void *pvParameters ) {
+    while (1) {
+        ulTaskNotifyTake(pdTRUE,portMAX_DELAY);
+        GPIOPinWrite(GPIO_PORTL_BASE, GPIO_PIN_2, GPIO_PIN_2);
+
+        /* Calculated as 5 * SysCtlClockGet() / 3 for 5 seconds of delay */
+        SysCtlDelay(400000000);
+
+        GPIOPinWrite(GPIO_PORTL_BASE, GPIO_PIN_2, ~GPIO_PIN_2);
+    }
 
 }
 
@@ -38,11 +72,15 @@ void motorTask( void *pvParameters ) {
 //
 //*****************************************************************************
 uint32_t motorTaskInit(void) {
+
+    // Initialize GPIO
+    gpioInit();
+
     //
     // Create the motor task.
     //
     if(xTaskCreate(motorTask, (const portCHAR *)"QUEUE", MOTORTASKSTACKSIZE, NULL,
-                   tskIDLE_PRIORITY + PRIORITY_MOTOR_TASK, NULL) != pdTRUE)
+                   tskIDLE_PRIORITY + PRIORITY_MOTOR_TASK, &motorTaskHandle) != pdTRUE)
     {
         return(1);
     }
--- FreeRTOS/motor_task.h
+++ FreeRTOS/motor_task.h
@@ -9,6 +9,12 @@
 #define MOTOR_TASK_H_
 
 #include <stdio.h>
+#include <stdint.h>
+#include <stdbool.h>
+#include "inc/hw_memmap.h"
+#include "driverlib/gpio.h"
+#include "driverlib/pin_map.h"
+#include "driverlib/sysctl.h"
 #include "FreeRTOS.h"
 #include "task.h"
 #include "semphr.h"
